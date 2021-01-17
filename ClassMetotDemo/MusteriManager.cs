b6f4415062e3adb9ce8da5ef2fa80c127f9bf377
Kamil Can Engin <cilgin_jojuk_kamil@hotmail.com>
2021-01-17 19:28:25 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler, Ekleme işlemi başarıyla gerçekleşmiştir. ID : "+ musteri.Id);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler, Silme işlemi başarıyla gerçekleşmiştir. ID :"+ musteri.Id);
+        }
+        public void Guncelle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler, Günceleme işlemi başarıyla gerçekleşmiştir. ID :"+ musteri.Id);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+
+            musteri1.Id=1;
+            musteri1.Adi="Can";
+            musteri1.Soyadi="Engin";
+            musteri1.TcNo="12345678910";
+            musteri1.EPosta="canengin@canengin.com";
+
+            musteri2.Id=2;
+            musteri2.Adi="Engin";
+            musteri2.Soyadi="Can";
+            musteri2.TcNo="12345678910";
+            musteri2.EPosta="engincan@engincan.com";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            Console.WriteLine("-------------------------");
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+            Console.WriteLine("-------------------------");
+            musteriManager.Guncelle(musteri1);
+            musteriManager.Guncelle(musteri2);
+            
+            Musteri[] musteriler = new Musteri[]{musteri1,musteri2};
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Ad : " + musteri.Adi);
+                Console.WriteLine("Soyad : " + musteri.Soyadi);
+                Console.WriteLine("-------------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
